32c685cc92ceb21357351935dca6db23db2963fe
Caroline Winkler <carolinewinkler@Carolines-MBP.fios-router.home>
2019-01-09 17:18:51 -0500

    added registration form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,7 +7,26 @@ class Register extends Component {
   render(){
     return(
       <div>Register!
-        
+        <form id="register"
+              onSubmit={this.submitRegistration}>
+
+          <label>First Name: </label>
+          <input type="text" id="firstName" placeholder="First Name"></input>
+
+          <label>Last Name: </label>
+          <input type="text" id="lastName" placeholder="Last Name"></input>
+
+          <label>Email: </label>
+          <input type="text" id="email" placeholder="Email"></input>
+
+          <label>Password: </label>
+          <input type="text" id="password" placeholder="Password"></input>
+
+          <label>Confirm Password: </label>
+          <input type="text" id="confirmPassword" placeholder=""></input>
+
+          <input type="submit"/>
+        </form>
       </div>
     )
   }
