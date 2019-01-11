d248ecf7a148bbeb8a6742a6a58d3fbba00df995
Angelica Winkler <carolinewinkler@Carolines-MBP.fios-router.home>
2019-01-11 12:12:28 -0500

    added feature to delete players

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -39,6 +39,33 @@ class Roster extends Component {
     this.props.history.push('/player/new')
   }
 
+  deletePlayer = (event) => {
+    let token = localStorage.getItem('token');
+    let playerID = event.target.dataset.id;
+
+    let options = {
+      url: `https://players-api.developer.alchemy.codes/api/players/${playerID}`,
+      method: 'DELETE',
+      headers: {'Authorization' : `Bearer ${token}`}
+    }
+
+    request(options, function(error, response, body){
+      let res = JSON.parse(body)
+      console.log(res)
+      if (res.success){
+        this.deletePlayerFromState(playerID)
+      }
+    }.bind(this))
+  }
+
+  deletePlayerFromState = (playerID) => {
+    let playerIndex = this.state.players.findIndex(player => player.id === playerID)
+      this.setState({
+        players: [...this.state.players.slice(0, playerIndex), ...this.state.players.slice(playerIndex+1)]
+      })
+
+
+  }
 
   render(){
     if (!localStorage.getItem('token')){
@@ -49,7 +76,10 @@ class Roster extends Component {
         <h3>Your Roster</h3>
 
         {!this.state.players ? null : this.state.players.map((player, index) =>
-          <li key={index}>{player.last_name}, {player.first_name}: Rating - {player.rating} | Handedness - {player.handedness}</li>
+          <div>
+            <li key={index}>{player.last_name}, {player.first_name}: Rating - {player.rating} | Handedness - {player.handedness}</li>
+            <button onClick={this.deletePlayer} data-id={player.id} key={index+10}>delete player</button>
+          </div>
         )}
 
         <button onClick={this.handleClick}>add new player</button>
