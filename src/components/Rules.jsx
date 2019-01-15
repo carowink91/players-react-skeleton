e82ac8b038aff8d8bffb89571ebf785545fca285
Angelica Winkler <carolinewinkler@Carolines-MBP.fios-router.home>
2019-01-14 21:11:02 -0500

    added rules to Wager game

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -5,7 +5,8 @@ const Rules = () => (
     THE RULES: <br /> <br /> Your bingo card is close to a win!
     <br /> ~ <br /> 1 - Select the number of granpas you will wager on the game.
     You'll win or lose as many grandpas as you wager.
-    <br /> ~ <br /> 2 - Click "Draw" to get your two bingo pieces. <br /> ~
+    <br/> ~ <br/> 2 - You cannot wager more Grandpas than are in your roster.
+    <br /> ~ <br /> 3 - Click "Draw" to get your two bingo pieces. <br /> ~
     <br />Good luck!
   </div>
 );
--- src/containers/WagerInstructions.jsx
+++ src/containers/WagerInstructions.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Segment, Form, Radio } from 'semantic-ui-react';
 import request from 'request';
 import Rules from '../components/Rules';
-import { fetchGetPlayers } from '../Fetches';
+import {
+  fetchGetPlayers,
+  fetchPostNewPlayer,
+  fetchDeletePlayer,
+} from '../Fetches';
 import RadioButtons from '../components/RadioButtons';
 
 class WagerInstructions extends Component {
@@ -12,6 +16,7 @@ class WagerInstructions extends Component {
       players: null,
       wager: '1',
       bingoPieces: '',
+      gameState: 'draw',
     };
   }
 
@@ -31,13 +36,36 @@ class WagerInstructions extends Component {
     { text: 'Three Grandpas', value: 'three' },
   ];
 
-  setWager = event =>
-    this.setState({ wager: event.currentTarget.children[0].value });
+  setWager = (event) => {
+    const wager = event.currentTarget.children[0].value;
+    if (wager > this.state.players.length) {
+      this.setState({ wager, gameState: 'shortage' });
+    } else {
+      this.setState({ wager, gameState: 'draw' });
+    }
+  };
 
-  resetGame = () => this.setState({ bingoPieces: '' });
+  renderButton = () => {
+    switch (this.state.gameState) {
+      case 'draw':
+        return 'Draw';
+      case 'reset':
+        return 'Reset Game';
+      case 'shortage':
+        return 'Not enough Grandpas';
+      default:
+    }
+  };
 
-  drawBingo = () => {
-    if (this.state.bingoPieces) {
+  handleClick = () => {
+    if (this.state.wager > this.state.players.length) {
+      return this.setState({ gameState: 'shortage' });
+    }
+    if (this.state.gameState === 'shortage') {
+      return;
+    }
+    if (this.state.gameState === 'reset') {
+      this.setState({ bingoPieces: '', gameState: 'draw' });
       return;
     }
     const bingoPieces = [
@@ -48,7 +76,70 @@ class WagerInstructions extends Component {
     ];
     const randomIndex = Math.floor(Math.random() * 4);
     const pieces = bingoPieces[randomIndex];
-    this.setState({ bingoPieces: pieces });
+    this.setState({ bingoPieces: pieces, gameState: 'reset' }, () => {
+      this.gainOrLoseGrandpas();
+    });
+  };
+
+  gainOrLoseGrandpas = () => {
+    if (
+      this.state.bingoPieces === ' I-27 and O-74' ||
+      this.state.bingoPieces === ' I-30 and O-64'
+    ) {
+      switch (this.state.wager) {
+        case '1':
+          this.gainGrandpas();
+          break;
+        case '2':
+          this.gainGrandpas();
+          this.gainGrandpas();
+          break;
+        case '3':
+          this.gainGrandpas();
+          this.gainGrandpas();
+          this.gainGrandpas();
+      }
+    } else {
+      switch (this.state.wager) {
+        case '1':
+          this.setState({ players: [...this.state.players.slice(1)] });
+          this.loseGrandpas(this.state.players[0].id);
+          break;
+        case '2':
+          this.setState({ players: [...this.state.players.slice(2)] });
+          this.loseGrandpas(this.state.players[0].id);
+          this.loseGrandpas(this.state.players[1].id);
+          break;
+        case '3':
+          this.setState({ players: [...this.state.players.slice(3)] });
+          this.loseGrandpas(this.state.players[0].id);
+          this.loseGrandpas(this.state.players[1].id);
+          this.loseGrandpas(this.state.players[2].id);
+      }
+    }
+  };
+
+  loseGrandpas = (id) => {
+    const token = localStorage.getItem('token');
+
+    fetchDeletePlayer(token, id, (errors, response, body) => {
+      const res = JSON.parse(body);
+      console.log(res);
+    });
+  };
+
+  gainGrandpas = () => {
+    const randomNum = (Math.floor(Math.random() * 9999999) + 1).toString();
+    const body = {
+      first_name: randomNum,
+      last_name: 'Bonus Grandpa',
+      rating: '9999',
+      handedness: 'left',
+    };
+    fetchPostNewPlayer(body, (errors, response, body) => {
+      const res = JSON.parse(body);
+      console.log(res);
+    });
   };
 
   getInstructions = () => {
@@ -69,18 +160,14 @@ class WagerInstructions extends Component {
     return (
       <div id="wager-column">
         <Rules />
-        <Segment style={{ background: 'rgb(203, 100, 96)' }}>
+        <Segment raised style={{ background: 'rgb(203, 100, 96)' }}>
           <RadioButtons
             setWager={this.setWager}
             currentWager={this.state.wager}
           />
 
-          <button id="draw-button" onClick={this.drawBingo}>
-            Draw
-          </button>
-
-          <button id="reset-button" onClick={this.resetGame}>
-            Click to Reset Game
+          <button id="draw-button" onClick={this.handleClick}>
+            {this.renderButton()}
           </button>
 
           <div id="bingo-piece">
